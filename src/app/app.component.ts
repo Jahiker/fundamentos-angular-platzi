313d4b532a6a271e34b028fe7d07a0a4f09e317b
Jahiker <rojasjahiker@gmail.com>
2022-08-12 00:28:57 -0400

    Interpolacion de propiedades

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -5,6 +5,9 @@ import { Component } from '@angular/core';
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
+
 export class AppComponent {
-  title = 'my-store';
+  public name: string = 'Jahiker';
+  age: number = 36;
+  img = 'https://source.unsplash.com/random';
 }
