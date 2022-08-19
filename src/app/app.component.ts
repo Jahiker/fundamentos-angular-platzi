23787a0ca9a999ede6fd1472027fff68bec4621f
Jahiker <rojasjahiker@gmail.com>
2022-08-19 00:46:48 -0400

    Bind data on properties and atributes and bind events

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,7 +7,33 @@ import { Component } from '@angular/core';
 })
 
 export class AppComponent {
+  // Propiedades
   public name: string = 'Jahiker';
   age: number = 36;
   img = 'https://source.unsplash.com/random';
+  btnDisabled: boolean = true;
+  person = {
+    name: "Jahiker",
+    age: 36,
+    avatar: "https://source.unsplash.com/random"
+  }
+
+  // Metodos
+  public toggleButton() {
+    this.btnDisabled = !this.btnDisabled;
+  }
+
+  increaseAge() {
+    this.person.age += 1;
+  }
+
+  onScroll(event: Event) {
+    const element = event.target as HTMLElement;
+    console.log("🚀 ~ file: app.component.ts ~ line 32 ~ AppComponent ~ onScroll ~ element", element.scrollTop)
+  }
+
+  changeName(event: Event) {
+    const element = event.target as HTMLInputElement;
+    this.person.name = element.value;
+  }
 }
